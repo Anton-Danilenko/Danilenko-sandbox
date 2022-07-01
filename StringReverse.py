08ebf2da89ba4e5c6a53ad963e88a6366f7f3ca3
Anton Danilenka <anton.danilenko@redbarkgames.com>
2022-07-01 17:17:04 +0300

    Переворот строки. Подправил

--- StringReverse.py
+++ StringReverse.py
@@ -1,11 +1,14 @@
+print ("Введите строку")
 s = input()
 i = 0
 cnt = 0
+new = ""
 while i <= len(s) - 1:
     if len(s) == 1:
         print(s[i])
         break
     else:
-        print(s[len(s) - 1 - cnt], sep='', end='')
+        new = new + s[len(s) - 1 - cnt]
         i += 1
         cnt += 1
+print (new)
