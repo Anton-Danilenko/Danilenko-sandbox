8ff3eb366bde4a27ddb69849e3264808bfbc954e
Anton Danilenka <anton.danilenko@redbarkgames.com>
2022-07-01 17:11:43 +0300

    Удаление символа из строки. Поправил

--- DelSymbFromString.py
+++ DelSymbFromString.py
@@ -2,11 +2,13 @@ print("Введите строку")
 s = input()
 print("Введите символ, который нужно удалить")
 x = input()
+new = ""
 i = 0
 cnt = 0
 while i <= len(s) - 1:
     if s[i] != x:
-        print(s[i], sep='', end='')
+        new = new + s[i]
         i += 1
     else:
         i += 1
+print (new)
